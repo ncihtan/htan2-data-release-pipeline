540a5e10d1753ae46546436944b55302910b4354
Yamina Katariya <ykatariy@dandelion.local>
2025-10-23 10:57:08 -0700

    include silver layer in run

--- htan-data-release-pipeline/run.py
+++ htan-data-release-pipeline/run.py
@@ -48,23 +48,23 @@ BRONZE_LAYER = "htan2_medallion_bronze"
 SILVER_LAYER = "htan2_medallion_silver"
 GOLD_LAYER = "htan2_medallion_gold"
 
-# try:
-#     print_section("HTAN DATA MODEL")
-#     htan_schema = data_model_load.main()
-# except Exception as e:
-#     print("Failed to load HTAN Data Model")
+try:
+    print_section("HTAN DATA MODEL")
+    htan_schema = data_model_load.main()
+except Exception as e:
+    print("Failed to load HTAN Data Model")
 
-# try:
-#     print_section("LEVEL: RAW TO BRONZE")
-#     r2b.main(HTAN_BQ_PROJECT, BRONZE_LAYER)
-# except Exception as e:
-#     print("Failed to run RAW TO BRONZE pipeline.")
+try:
+    print_section("LEVEL: RAW TO BRONZE")
+    r2b.main(HTAN_BQ_PROJECT, BRONZE_LAYER)
+except Exception as e:
+    print("Failed to run RAW TO BRONZE pipeline.")
 
-# try:
-print_section("LEVEL: BRONZE TO SILVER")
-b2s.main(HTAN_BQ_PROJECT, SILVER_LAYER, BRONZE_LAYER)
-# except Exception as e:
-#     print("Failed to run BRONZE TO SILVER pipeline.")
+try:
+    print_section("LEVEL: BRONZE TO SILVER")
+    b2s.main(HTAN_BQ_PROJECT, SILVER_LAYER, BRONZE_LAYER)
+except Exception as e:
+    print("Failed to run BRONZE TO SILVER pipeline.")
 
 # try:
 #     print_section("LEVEL: SILVER TO GOLD")
@@ -73,4 +73,4 @@ b2s.main(HTAN_BQ_PROJECT, SILVER_LAYER, BRONZE_LAYER)
 #     print("Failed to run SILVER TO GOLD pipeline.")
 
 # Final clean-up
-# data_model_load.remove_data_model_files()
+data_model_load.remove_data_model_files()
