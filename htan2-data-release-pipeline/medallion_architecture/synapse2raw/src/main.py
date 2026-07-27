a592e80c88f78dbe768e21eadc0f4f61f8198484
PozhidayevaDarya <106110175+PozhidayevaDarya@users.noreply.github.com>
2026-07-27 13:59:19 -0700

    Fix edited date

--- htan2-data-release-pipeline/medallion_architecture/synapse2raw/src/main.py
+++ htan2-data-release-pipeline/medallion_architecture/synapse2raw/src/main.py
@@ -15,7 +15,7 @@ Requires (env):
   - SCHEMA_BINDING_CONFIG_URL (defaults to htan2_project_setup raw URL)
 
 Authors: Dar'ya Pozhidayeva
-Updated: 2026-06-17
+Updated: 2026-07-27
 """
 
 from __future__ import annotations
